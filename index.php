0e2ebf1604fdc8544ce3ba05466af7c662baa78c
arturu <arturu@arturu-MacBookPro>
2014-01-04 01:58:06 +0100

    inizio

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @version     0.1
+ * @package	Core
+ * @copyright	Copyright (C) 2003 - 2013 Open Source. All rights reserved.
+ * @license	GNU/GPL version 3
+ * @author 	Pietro Arturo Panetta arturopanetta@gmail.com
+ * @todo	Concerto è un software modulare per lo sviluppo di applicazioni web.
+ */
