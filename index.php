7d860945695f1c214a44af13903656fd275968dd
arturu <arturu@arturu-MacBookPro>
2014-01-12 23:05:15 +0100

    http://blog.arturu.it/libro/sviluppo-php-un-framework-didattico/60/il-primo-pattern-mvc

--- index.php
+++ index.php
@@ -7,9 +7,7 @@
  * @author 	Pietro Arturo Panetta arturopanetta@gmail.com
  * @todo	Concerto è un software modulare per lo sviluppo di applicazioni web.
  */
-
-    // Path assoluta senza DIRECTORY_SEPARATOR finale
-    define( 'PATH_BASE' , dirname(__FILE__) );
     
-    // carico l'Autoload delle classi
-    require_once( PATH_BASE . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'Autoloader.php' );
+// carico l'Autoload delle classi
+require_once( 'app' . DIRECTORY_SEPARATOR . 'Autoloader.php' );
+
