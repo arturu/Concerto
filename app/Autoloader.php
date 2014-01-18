01ffe39774469f06b87341d60eb4db1ce624e69a
arturu <arturu@arturu-MacBookPro>
2014-01-18 22:43:54 +0100

    http://blog.arturu.it/libro/sviluppo-php-concerto-un-framework-didattico/64/namespaces

--- app/Autoloader.php
+++ app/Autoloader.php
@@ -2,33 +2,30 @@
 /**
  * Autoloader di classi
  * 
- * @version	0.1
- * @package	Core
+ * @version	0.2
+ * @package	Autoload
  * @file 	Autoloader di classi.
  * 
- * @todo    Dato il nome della classe (Cartella_Classe) cerco i file in vari percosi
- *          sostituendo "_" con DIRECTORY_SEPARATOR.
- *          Potrei usare RecursiveDirectoryIterator ma dato che i possibili percorsi sono 
- *          pochi utilizzo questa soluzione, di gran lunga più veloce che scandire
- *          tutti i file e le sottocartelle in "app"
+ * @todo        Dato il nome della classe (namespace\Classe) carico la relativa classe.
  */
 spl_autoload_register(
-    function($classe) { // http://www.php.net/manual/it/functions.anonymous.php
+    function($classe) {
 
         // la cartella base delle app
-        $base = dirname(__FILE__) . DIRECTORY_SEPARATOR ;
+        $base = __DIR__ . DIRECTORY_SEPARATOR ;
 
-        // sostituisco "_" con DIRECTORY_SEPARATOR in modo da caricare classi dentro le cartelle
-        $classe = str_replace('_', DIRECTORY_SEPARATOR, $classe);
+        // sostituisco "\" con DIRECTORY_SEPARATOR in modo da caricare classi dentro le cartelle
+        $classe = str_replace('\\', DIRECTORY_SEPARATOR, $classe);
 
         // percorso completo del file da includere
         $file = $base . $classe . '.php';
         
         // controllo se esiste ed è leggibile
-        if ( file_exists($file) && is_readable($file)) {
+        if ( file_exists($file) && is_readable($file) ) {
             require_once $file;
             return true;
         }
+        
         return false;
 
     });
