773d1229970d39f62c61053a460848677fb4bec7
arturu <arturu@arturu-MacBookPro>
2014-01-19 14:25:14 +0100

    http://blog.arturu.it/libro/sviluppo-php-concerto-un-framework-didattico/65/gestione-delle-eccezioni

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <?php
 /**
- * @version     0.2
- * @package	index
- * @copyright	Copyright (C) 2003 - 2013 Open Source. All rights reserved.
- * @license	GNU/GPL version 3
- * @author 	Pietro Arturo Panetta arturopanetta@gmail.com
- * @todo	Concerto è un software modulare per lo sviluppo di applicazioni web.
+ * Index dell'applicazione
+ * 
+ * @version 0.2
+ * @package index
+ * @copyright Copyright (C) 2003 - 2013 Open Source. All rights reserved.
+ * @license GNU/GPL version 3
+ * @author Pietro Arturo Panetta arturopanetta@gmail.com
+ * @todo Concerto è un software modulare per lo sviluppo di applicazioni web.
  */
   
+try {
     // carico l'Autoload delle classi
     require_once( 'app' . DIRECTORY_SEPARATOR . 'Autoloader.php' );
 
+    // avvio del Core
     Concerto\Core::run();
+    
+} catch (Concerto\Eccezione $e) { // Oggetto Eccezione
+    
+    // stampa dell'errore
+    $e->stampa($e);
+    
+}
